60c2af50ae5bc01ed869788b3b928af55400d8a9
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-18 11:16:47 -0700

    Update Sample

--- samples/CommunityToolkit.Maui.Markup.Sample/ViewModels/Base/BaseViewModel.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/ViewModels/Base/BaseViewModel.cs
@@ -4,31 +4,30 @@ using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using AsyncAwaitBestPractices;
 
-namespace CommunityToolkit.Maui.Markup.Sample.ViewModels.Base
+namespace CommunityToolkit.Maui.Markup.Sample.ViewModels.Base;
+
+abstract class BaseViewModel : INotifyPropertyChanged
 {
-    abstract class BaseViewModel : INotifyPropertyChanged
+    readonly WeakEventManager _propertyChangedEventManager = new();
+
+    event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged
     {
-        readonly WeakEventManager _propertyChangedEventManager = new();
-
-        event PropertyChangedEventHandler? INotifyPropertyChanged.PropertyChanged
-        {
-            add => _propertyChangedEventManager.AddEventHandler(value);
-            remove => _propertyChangedEventManager.RemoveEventHandler(value);
-        }
-
-        protected void SetProperty<T>(ref T backingStore, in T value, in Action? onChanged = null, [CallerMemberName] in string propertyname = "")
-        {
-            if (EqualityComparer<T>.Default.Equals(backingStore, value))
-                return;
-
-            backingStore = value;
-
-            onChanged?.Invoke();
-
-            OnPropertyChanged(propertyname);
-        }
-
-        void OnPropertyChanged([CallerMemberName] in string propertyName = "") =>
-            _propertyChangedEventManager.RaiseEvent(this, new PropertyChangedEventArgs(propertyName), nameof(INotifyPropertyChanged.PropertyChanged));
+        add => _propertyChangedEventManager.AddEventHandler(value);
+        remove => _propertyChangedEventManager.RemoveEventHandler(value);
     }
+
+    protected void SetProperty<T>(ref T backingStore, in T value, in Action? onChanged = null, [CallerMemberName] in string propertyname = "")
+    {
+        if (EqualityComparer<T>.Default.Equals(backingStore, value))
+            return;
+
+        backingStore = value;
+
+        onChanged?.Invoke();
+
+        OnPropertyChanged(propertyname);
+    }
+
+    void OnPropertyChanged([CallerMemberName] in string propertyName = "") =>
+        _propertyChangedEventManager.RaiseEvent(this, new PropertyChangedEventArgs(propertyName), nameof(INotifyPropertyChanged.PropertyChanged));
 }
--- samples/CommunityToolkit.Maui.Markup.Sample/ViewModels/NewsViewModel.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/ViewModels/NewsViewModel.cs
@@ -13,108 +13,107 @@ using CommunityToolkit.Maui.Markup.Sample.ViewModels.Base;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Essentials;
 
-namespace CommunityToolkit.Maui.Markup.Sample.ViewModels
+namespace CommunityToolkit.Maui.Markup.Sample.ViewModels;
+
+class NewsViewModel : BaseViewModel
 {
-    class NewsViewModel : BaseViewModel
+    readonly WeakEventManager<string> _pullToRefreshEventManager = new();
+    readonly HackerNewsAPIService _hackerNewsAPIService;
+
+    bool _isListRefreshing;
+
+    public NewsViewModel(HackerNewsAPIService hackerNewsAPIService)
     {
-        readonly WeakEventManager<string> _pullToRefreshEventManager = new();
-        readonly HackerNewsAPIService _hackerNewsAPIService;
+        _hackerNewsAPIService = hackerNewsAPIService;
 
-        bool _isListRefreshing;
-
-        public NewsViewModel(HackerNewsAPIService hackerNewsAPIService)
-        {
-            _hackerNewsAPIService = hackerNewsAPIService;
-
-            RefreshCommand = new AsyncCommand(ExecuteRefreshCommand);
-
-            //Ensure Observable Collection is thread-safe https://codetraveler.io/2019/09/11/using-observablecollection-in-a-multi-threaded-xamarin-forms-application/
-            BindingBase.EnableCollectionSynchronization(TopStoryCollection, null, ObservableCollectionCallback);
-        }
-
-        public event EventHandler<string> PullToRefreshFailed
-        {
-            add => _pullToRefreshEventManager.AddEventHandler(value);
-            remove => _pullToRefreshEventManager.RemoveEventHandler(value);
-        }
-
-        public ObservableCollection<StoryModel> TopStoryCollection { get; } = new ObservableCollection<StoryModel>();
-
-        public ICommand RefreshCommand { get; }
-
-        public bool IsListRefreshing
-        {
-            get => _isListRefreshing;
-            set => SetProperty(ref _isListRefreshing, value);
-        }
-
-        static void InsertIntoSortedCollection<T>(ObservableCollection<T> collection, Comparison<T> comparison, T modelToInsert)
-        {
-            if (collection.Count is 0)
-            {
-                collection.Add(modelToInsert);
-            }
-            else
-            {
-                int index = 0;
-                foreach (var model in collection)
-                {
-                    if (comparison(model, modelToInsert) >= 0)
-                    {
-                        collection.Insert(index, modelToInsert);
-                        return;
-                    }
-
-                    index++;
-                }
-
-                collection.Insert(index, modelToInsert);
-            }
-        }
-
-        async Task ExecuteRefreshCommand()
-        {
-            TopStoryCollection.Clear();
-
-            try
-            {
-                await foreach (var story in GetTopStories(50).ConfigureAwait(false))
-                {
-                    if (story is not null && !TopStoryCollection.Any(x => x.Title.Equals(story.Title)))
-                        InsertIntoSortedCollection(TopStoryCollection, (a, b) => b.Score.CompareTo(a.Score), story);
-                }
-            }
-            catch (Exception e)
-            {
-                OnPullToRefreshFailed(e.ToString());
-            }
-            finally
-            {
-                IsListRefreshing = false;
-            }
-        }
-
-        async IAsyncEnumerable<StoryModel> GetTopStories(int? storyCount = int.MaxValue)
-        {
-            var topStoryIds = await _hackerNewsAPIService.GetTopStoryIDs().ConfigureAwait(false);
-            var getTopStoryTaskList = topStoryIds.Select(_hackerNewsAPIService.GetStory).ToList();
-
-            while (getTopStoryTaskList.Any() && storyCount-- > 0)
-            {
-                var completedGetStoryTask = await Task.WhenAny(getTopStoryTaskList).ConfigureAwait(false);
-                getTopStoryTaskList.Remove(completedGetStoryTask);
-
-                var story = await completedGetStoryTask.ConfigureAwait(false);
-                yield return story;
-            }
-        }
+        RefreshCommand = new AsyncCommand(ExecuteRefreshCommand);
 
         //Ensure Observable Collection is thread-safe https://codetraveler.io/2019/09/11/using-observablecollection-in-a-multi-threaded-xamarin-forms-application/
-        void ObservableCollectionCallback(IEnumerable collection, object context, Action accessMethod, bool writeAccess)
-        {
-            MainThread.BeginInvokeOnMainThread(accessMethod);
-        }
-
-        void OnPullToRefreshFailed(string message) => _pullToRefreshEventManager.RaiseEvent(this, message, nameof(PullToRefreshFailed));
+        BindingBase.EnableCollectionSynchronization(TopStoryCollection, null, ObservableCollectionCallback);
     }
+
+    public event EventHandler<string> PullToRefreshFailed
+    {
+        add => _pullToRefreshEventManager.AddEventHandler(value);
+        remove => _pullToRefreshEventManager.RemoveEventHandler(value);
+    }
+
+    public ObservableCollection<StoryModel> TopStoryCollection { get; } = new ObservableCollection<StoryModel>();
+
+    public ICommand RefreshCommand { get; }
+
+    public bool IsListRefreshing
+    {
+        get => _isListRefreshing;
+        set => SetProperty(ref _isListRefreshing, value);
+    }
+
+    static void InsertIntoSortedCollection<T>(ObservableCollection<T> collection, Comparison<T> comparison, T modelToInsert)
+    {
+        if (collection.Count is 0)
+        {
+            collection.Add(modelToInsert);
+        }
+        else
+        {
+            int index = 0;
+            foreach (var model in collection)
+            {
+                if (comparison(model, modelToInsert) >= 0)
+                {
+                    collection.Insert(index, modelToInsert);
+                    return;
+                }
+
+                index++;
+            }
+
+            collection.Insert(index, modelToInsert);
+        }
+    }
+
+    async Task ExecuteRefreshCommand()
+    {
+        TopStoryCollection.Clear();
+
+        try
+        {
+            await foreach (var story in GetTopStories(50).ConfigureAwait(false))
+            {
+                if (story is not null && !TopStoryCollection.Any(x => x.Title.Equals(story.Title)))
+                    InsertIntoSortedCollection(TopStoryCollection, (a, b) => b.Score.CompareTo(a.Score), story);
+            }
+        }
+        catch (Exception e)
+        {
+            OnPullToRefreshFailed(e.ToString());
+        }
+        finally
+        {
+            IsListRefreshing = false;
+        }
+    }
+
+    async IAsyncEnumerable<StoryModel> GetTopStories(int? storyCount = int.MaxValue)
+    {
+        var topStoryIds = await _hackerNewsAPIService.GetTopStoryIDs().ConfigureAwait(false);
+        var getTopStoryTaskList = topStoryIds.Select(_hackerNewsAPIService.GetStory).ToList();
+
+        while (getTopStoryTaskList.Any() && storyCount-- > 0)
+        {
+            var completedGetStoryTask = await Task.WhenAny(getTopStoryTaskList).ConfigureAwait(false);
+            getTopStoryTaskList.Remove(completedGetStoryTask);
+
+            var story = await completedGetStoryTask.ConfigureAwait(false);
+            yield return story;
+        }
+    }
+
+    //Ensure Observable Collection is thread-safe https://codetraveler.io/2019/09/11/using-observablecollection-in-a-multi-threaded-xamarin-forms-application/
+    void ObservableCollectionCallback(IEnumerable collection, object context, Action accessMethod, bool writeAccess)
+    {
+        MainThread.BeginInvokeOnMainThread(accessMethod);
+    }
+
+    void OnPullToRefreshFailed(string message) => _pullToRefreshEventManager.RaiseEvent(this, message, nameof(PullToRefreshFailed));
 }
