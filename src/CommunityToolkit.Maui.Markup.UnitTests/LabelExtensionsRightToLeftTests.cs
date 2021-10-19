c52351ec3dd5307e68e0e8cf1014dbeaa6949b7b
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-10-19 15:35:44 -0700

    Use File-Scoped Namespaces (#11)
    * Consolidate Files
    
    * Update Info.plist
    
    * Update Unit Tests
    
    * Update Sample

--- src/CommunityToolkit.Maui.Markup.UnitTests/LabelExtensionsRightToLeftTests.cs
+++ src/CommunityToolkit.Maui.Markup.UnitTests/LabelExtensionsRightToLeftTests.cs
@@ -3,22 +3,21 @@ using CommunityToolkit.Maui.Markup.RightToLeft;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui;
 
-namespace CommunityToolkit.Maui.Markup.UnitTests
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+[TestFixture]
+public class LabelExtensionsRightToLeftTests : MarkupBaseTestFixture<Label>
 {
-    [TestFixture]
-    public class LabelExtensionsRightToLeftTests : MarkupBaseTestFixture<Label>
-    {
-        [Test]
-        public void TextLeft()
-            => TestPropertiesSet(l => l?.TextLeft(), (Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.End));
+    [Test]
+    public void TextLeft()
+        => TestPropertiesSet(l => l?.TextLeft(), (Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.End));
 
-        [Test]
-        public void TextRight()
-            => TestPropertiesSet(l => l?.TextRight(), (Label.HorizontalTextAlignmentProperty, TextAlignment.End, TextAlignment.Start));
+    [Test]
+    public void TextRight()
+        => TestPropertiesSet(l => l?.TextRight(), (Label.HorizontalTextAlignmentProperty, TextAlignment.End, TextAlignment.Start));
 
-        [Test]
-        public void SupportDerivedFromLabel() => Assert.IsInstanceOf<DerivedFromLabel>(new DerivedFromLabel().TextLeft().TextRight());
+    [Test]
+    public void SupportDerivedFromLabel() => Assert.IsInstanceOf<DerivedFromLabel>(new DerivedFromLabel().TextLeft().TextRight());
 
-        class DerivedFromLabel : Label { }
-    }
+    class DerivedFromLabel : Label { }
 }
