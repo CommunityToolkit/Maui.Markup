f922975dff08ddc926feaf6f89313a102c97c626
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-03-22 13:17:40 -0700

    Rename Test Files

--- src/CommunityToolkit.Maui.Markup.UnitTests/LabelExtensionsTests.cs
+++ src/CommunityToolkit.Maui.Markup.UnitTests/LabelExtensionsTests.cs
@@ -1,58 +1,12 @@
-﻿using System;
-using CommunityToolkit.Maui.Markup.UnitTests.Base;
-using Microsoft.Maui;
+﻿using CommunityToolkit.Maui.Markup.UnitTests.Base;
 using Microsoft.Maui.Controls;
 using Microsoft.Maui.Graphics;
 using NUnit.Framework;
 
 namespace CommunityToolkit.Maui.Markup.UnitTests;
 
-[TestFixture]
 class LabelExtensionsTests : BaseMarkupTestFixture<Label>
 {
-	[Test]
-	public void TextStart()
-		=> TestPropertiesSet(l => l.TextStart(), (Label.HorizontalTextAlignmentProperty, TextAlignment.End, TextAlignment.Start));
-
-	[Test]
-	public void TextCenterHorizontal()
-		=> TestPropertiesSet(l => l.TextCenterHorizontal(), (Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center));
-
-	[Test]
-	public void TextEnd()
-		=> TestPropertiesSet(l => l.TextEnd(), (Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.End));
-
-	[Test]
-	public void TextTop()
-		=> TestPropertiesSet(l => l.TextTop(), (Label.VerticalTextAlignmentProperty, TextAlignment.End, TextAlignment.Start));
-
-	[Test]
-	public void TextCenterVertical()
-		=> TestPropertiesSet(l => l.TextCenterVertical(), (Label.VerticalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center));
-
-	[Test]
-	public void TextBottom()
-		=> TestPropertiesSet(l => l.TextBottom(), (Label.VerticalTextAlignmentProperty, TextAlignment.Start, TextAlignment.End));
-
-	[Test]
-	public void TextCenter()
-		=> TestPropertiesSet(
-				l => l.TextCenter(),
-				(Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center),
-				(Label.VerticalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center));
-
-	[Test]
-	public void FontSize()
-		=> TestPropertiesSet(l => l.FontSize(8.0), (Label.FontSizeProperty, 6.0, 8.0));
-
-	[Test]
-	public void Bold()
-		=> TestPropertiesSet(l => l.Bold(), (Label.FontAttributesProperty, FontAttributes.None, FontAttributes.Bold));
-
-	[Test]
-	public void Italic()
-		=> TestPropertiesSet(l => l.Italic(), (Label.FontAttributesProperty, FontAttributes.None, FontAttributes.Italic));
-
 	[Test]
 	public void FormattedTextSingleSpan()
 	{
@@ -98,3 +52,4 @@ class LabelExtensionsTests : BaseMarkupTestFixture<Label>
 
 	class DerivedFromLabel : Label { }
 }
+
---
+++ src/CommunityToolkit.Maui.Markup.UnitTests/TextAlignmentExtensionsTests.cs
@@ -0,0 +1,72 @@
+﻿using CommunityToolkit.Maui.Markup.UnitTests.Base;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+[TestFixture]
+class TextAlignmentExtensionsTests : BaseMarkupTestFixture<Picker>
+{
+	[Test]
+	public void TextStart()
+		=> TestPropertiesSet(l => l.TextStart(), (Label.HorizontalTextAlignmentProperty, TextAlignment.End, TextAlignment.Start));
+
+	[Test]
+	public void TextCenterHorizontal()
+		=> TestPropertiesSet(l => l.TextCenterHorizontal(), (Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center));
+
+	[Test]
+	public void TextEnd()
+		=> TestPropertiesSet(l => l.TextEnd(), (Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.End));
+
+	[Test]
+	public void TextTop()
+		=> TestPropertiesSet(l => l.TextTop(), (Label.VerticalTextAlignmentProperty, TextAlignment.End, TextAlignment.Start));
+
+	[Test]
+	public void TextCenterVertical()
+		=> TestPropertiesSet(l => l.TextCenterVertical(), (Label.VerticalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center));
+
+	[Test]
+	public void TextBottom()
+		=> TestPropertiesSet(l => l.TextBottom(), (Label.VerticalTextAlignmentProperty, TextAlignment.Start, TextAlignment.End));
+
+	[Test]
+	public void TextCenter()
+		=> TestPropertiesSet(
+				l => l.TextCenter(),
+				(Label.HorizontalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center),
+				(Label.VerticalTextAlignmentProperty, TextAlignment.Start, TextAlignment.Center));
+
+	[Test]
+	public void FontSize()
+		=> TestPropertiesSet(l => l.FontSize(8.0), (Label.FontSizeProperty, 6.0, 8.0));
+
+	[Test]
+	public void Bold()
+		=> TestPropertiesSet(l => l.Bold(), (Label.FontAttributesProperty, FontAttributes.None, FontAttributes.Bold));
+
+	[Test]
+	public void Italic()
+		=> TestPropertiesSet(l => l.Italic(), (Label.FontAttributesProperty, FontAttributes.None, FontAttributes.Italic));
+
+	[Test]
+	public void SupportDerivedFromBindable()
+	{
+		Assert.IsInstanceOf<DerivedFromLabel>(
+			new DerivedFromLabel()
+			.TextStart()
+			.TextCenterHorizontal()
+			.TextEnd()
+			.TextTop()
+			.TextCenterVertical()
+			.TextBottom()
+			.TextCenter()
+			.FontSize(8.0)
+			.Bold()
+			.Italic());
+	}
+
+	class DerivedFromLabel : Picker { }
+}
