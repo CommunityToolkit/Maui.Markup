94d087f8edebf0024badda558e23e031376d9366
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-04-02 18:52:10 -0700

    Add `PlaceholderExtensions`

---
+++ src/CommunityToolkit.Maui.Markup.UnitTests/PaceholderExtensionsTests.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Maui.Markup.UnitTests.Base;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+class PaceholderExtensionsTests : BaseMarkupTestFixture<Entry>
+{
+	[Test]
+	public void SetPlaceholderTest()
+		=> TestPropertiesSet(e => e.Placeholder("Hello World"), (PlaceholderElement.PlaceholderProperty, "Hello World"));
+
+	[Test]
+	public void SetPlaceholderAndColorTest()
+		=> TestPropertiesSet(e => e.Placeholder("Hello World", Colors.Red), (PlaceholderElement.PlaceholderProperty, "Hello World"), (PlaceholderElement.PlaceholderColorProperty, Colors.Red));
+
+	[Test]
+	public void SetPlaceholderColorTest()
+		=> TestPropertiesSet(e => e.PlaceholderColor(Colors.Red), (PlaceholderElement.PlaceholderColorProperty, Colors.Red));
+}
+
