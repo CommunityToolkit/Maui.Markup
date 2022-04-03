18bf976493b7762479499ad0a53456b72a69e456
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-04-02 19:42:06 -0700

    Add `ImageExtensions`

---
+++ src/CommunityToolkit.Maui.Markup.UnitTests/ImageExtensionTests.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Maui.Markup.UnitTests.Base;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+class ImageExtensionTests : BaseMarkupTestFixture<Image>
+{
+	const string resourceToLoad = "CommunityToolkit.Maui.UnitTests.Resources.dotnet-bot.png";
+
+	[Test]
+	public void SetSourceTest()
+	{
+		var image = new Image();
+
+		Assert.Null(image.Source);
+		Assert.IsNotInstanceOf<FileImageSource>(image.Source);
+
+		image.Source(resourceToLoad);
+
+		Assert.IsInstanceOf<FileImageSource>(image.Source);
+		Assert.False(image.Source.IsEmpty);
+	}
+
+	[Test]
+	public void SetAspectTest()
+	{
+		TestPropertiesSet(i => i.Aspect(Aspect.AspectFill), (ImageElement.AspectProperty, Aspect.AspectFill));
+		TestPropertiesSet(i => i.Aspect(Aspect.Center), (ImageElement.AspectProperty, Aspect.Center));
+		TestPropertiesSet(i => i.Aspect(Aspect.Fill), (ImageElement.AspectProperty, Aspect.Fill));
+	}
+
+	[Test]
+	public void SetIsOpaqueTest()
+		=> TestPropertiesSet(i => i.IsOpaque(true), (ImageElement.IsOpaqueProperty, true));
+
+	[Test]
+	public void SupportDerivedFromLabel()
+	{
+		Assert.IsInstanceOf<DerivedFromImage>(
+			new DerivedFromImage()
+			.Source(resourceToLoad)
+			.Aspect(Aspect.Center)
+			.IsOpaque(true));
+	}
+
+	class DerivedFromImage : Image
+	{
+
+	}
+}
---
+++ src/CommunityToolkit.Maui.Markup/ImageExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace CommunityToolkit.Maui.Markup;
+
+/// <summary>
+/// Extension methods for IImage
+/// </summary>
+public static class ImageExtensions
+{
+	/// <summary>
+	/// Sets the <see cref="IImageSourcePart.Source" /> Property/>
+	/// </summary>
+	/// <typeparam name="TBindable"><see cref="BindableObject"/> : <see cref="IImageSourcePart"/></typeparam>
+	/// <param name="bindable">The <see cref="BindableObject"/> on which to set the <see cref="IImageSourcePart.Source"/> Property</param>
+	/// <param name="imageSource">The <see cref="Microsoft.Maui.Controls.ImageSource"/> value</param>
+	/// <returns></returns>
+	public static TBindable Source<TBindable>(this TBindable bindable, ImageSource? imageSource) where TBindable : BindableObject, IImageSourcePart
+	{
+		bindable.SetValue(ImageElement.SourceProperty, imageSource);
+		return bindable;
+	}
+
+	/// <summary>
+	/// Sets the <see cref="IImage.Aspect" /> Property/>
+	/// </summary>
+	/// <typeparam name="TBindable"><see cref="BindableObject"/> : <see cref="IImage"/></typeparam>
+	/// <param name="bindable">The <see cref="BindableObject"/> on which to set the <see cref="IImage.Aspect"/> Property</param>
+	/// <param name="aspect">The <see cref="Microsoft.Maui.Aspect"/> vaue</param>
+	/// <returns></returns>
+	public static TBindable Aspect<TBindable>(this TBindable bindable, Aspect aspect) where TBindable : BindableObject, IImage
+	{
+		bindable.SetValue(ImageElement.AspectProperty, aspect);
+		return bindable;
+	}
+
+	/// <summary>
+	/// Sets the <see cref="IImage.IsOpaque" /> Property/>
+	/// </summary>
+	/// <typeparam name="TBindable"><see cref="BindableObject"/> : <see cref="IImage"/></typeparam>
+	/// <param name="bindable">The <see cref="BindableObject"/> on which to set the <see cref="IImage.IsOpaque"/> Property</param>
+	/// <param name="isOpaque">The <see cref="bool"/> vaue</param>
+	/// <returns></returns>
+	public static TBindable IsOpaque<TBindable>(this TBindable bindable, bool isOpaque) where TBindable : BindableObject, IImage
+	{
+		bindable.SetValue(ImageElement.IsOpaqueProperty, isOpaque);
+		return bindable;
+	}
+}
+
