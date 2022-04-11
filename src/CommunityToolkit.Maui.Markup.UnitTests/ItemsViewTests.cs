9a120832b38805a93890d4f77ba8140a3d31db5a
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-04-11 08:01:45 -0700

    Add `ItemsView` Extensions

---
+++ src/CommunityToolkit.Maui.Markup.UnitTests/ItemsViewTests.cs
@@ -0,0 +1,109 @@
+﻿using CommunityToolkit.Maui.Markup.UnitTests.Base;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+class ItemsViewTests : BaseMarkupTestFixture<CollectionView>
+{
+	[Test]
+	public void EmptyViewTest()
+	{
+		var view = new BoxView();
+		TestPropertiesSet(l => l.EmptyView(view), (ItemsView.EmptyViewProperty, view));
+	}
+
+	[Test]
+	public void EmptyViewTemplateTest()
+	{
+		var dataTemplate = new DataTemplate(() => new Label().Text("Hello World"));
+		TestPropertiesSet(l => l.EmptyViewTemplate(dataTemplate), (ItemsView.EmptyViewTemplateProperty, dataTemplate));
+	}
+
+	[Test]
+	public void ItemsSourceTest()
+	{
+		var itemSource = new[] { "Hello", "World" };
+		TestPropertiesSet(l => l.ItemsSource(itemSource), (ItemsView.ItemsSourceProperty, itemSource));
+	}
+
+	[Test]
+	public void HorizontalScrollBarVisibilityTest()
+	{
+		TestPropertiesSet(l => l.HorizontalScrollBarVisibility(ScrollBarVisibility.Always), (ItemsView.HorizontalScrollBarVisibilityProperty, ScrollBarVisibility.Always));
+	}
+
+	[Test]
+	public void VerticalScrollBarVisibilityTest()
+	{
+		TestPropertiesSet(l => l.VerticalScrollBarVisibility(ScrollBarVisibility.Always), (ItemsView.VerticalScrollBarVisibilityProperty, ScrollBarVisibility.Always));
+	}
+
+	[Test]
+	public void ScrollBarVisibilityTest()
+	{
+		TestPropertiesSet(l => l.ScrollBarVisibility(ScrollBarVisibility.Always), (ItemsView.VerticalScrollBarVisibilityProperty, ScrollBarVisibility.Always), (ItemsView.HorizontalScrollBarVisibilityProperty, ScrollBarVisibility.Always));
+	}
+
+	[Test]
+	public void RemainingItemsThresholdTest()
+	{
+		TestPropertiesSet(l => l.RemainingItemsThreshold(10), (ItemsView.RemainingItemsThresholdProperty, 10));
+	}
+
+	[Test]
+	public void RemainingItemsThresholdReachedCommandTest()
+	{
+		var command = new Command<string>(text => text = text[1..]);
+		TestPropertiesSet(l => l.RemainingItemsThresholdReachedCommand(command), (ItemsView.RemainingItemsThresholdReachedCommandProperty, command));
+	}
+
+	[Test]
+	public void RemainingItemsThresholdReachedCommandPlusParameterTest()
+	{
+		var command = new Command<string>(text => text = text[1..]);
+		TestPropertiesSet(l => l.RemainingItemsThresholdReachedCommand(command, "Hello"), (ItemsView.RemainingItemsThresholdReachedCommandProperty, command),
+																							(ItemsView.RemainingItemsThresholdReachedCommandParameterProperty, "Hello"));
+	}
+
+	[Test]
+	public void RemainingItemsThresholdReachedCommandParameterTest()
+	{
+		TestPropertiesSet(l => l.RemainingItemsThresholdReachedCommandParameter("Hello"), (ItemsView.RemainingItemsThresholdReachedCommandParameterProperty, "Hello"));
+	}
+
+	[Test]
+	public void ItemTemplateTest()
+	{
+		var dataTemplate = new DataTemplate(() => new Label().TextColor(Colors.Green));
+		TestPropertiesSet(l => l.ItemTemplate(dataTemplate), (ItemsView.ItemTemplateProperty, dataTemplate));
+	}
+
+	[Test]
+	public void ItemsUpdatingScrollModeTest()
+	{
+		TestPropertiesSet(l => l.ItemsUpdatingScrollMode(ItemsUpdatingScrollMode.KeepLastItemInView), (ItemsView.ItemsUpdatingScrollModeProperty, ItemsUpdatingScrollMode.KeepLastItemInView));
+	}
+
+	[Test]
+	public void DerivedFromCarouselViewTest()
+	{
+		Assert.IsInstanceOf<DerivedFromCarouselView>(
+			new DerivedFromCarouselView()
+			.EmptyView(new BoxView { BackgroundColor = Colors.Green })
+			.EmptyViewTemplate(new DataTemplate(() => new BoxView { BackgroundColor = Colors.Red }))
+			.ItemsSource(new[] { "Hello", "World" })
+			.ScrollBarVisibility(ScrollBarVisibility.Never)
+			.RemainingItemsThreshold(10)
+			.RemainingItemsThresholdReachedCommand(new Command<string>(text => text = text[..1]), "Hello World")
+			.ItemTemplate(new DataTemplate(() => new Label()))
+			.ItemsUpdatingScrollMode(ItemsUpdatingScrollMode.KeepScrollOffset));
+	}
+
+	class DerivedFromCarouselView : CarouselView
+	{
+
+	}
+}
---
+++ src/CommunityToolkit.Maui.Markup/ItemsViewExtensions.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Windows.Input;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace CommunityToolkit.Maui.Markup;
+
+/// <summary>
+/// Fluent extension methods for <see cref="ItemsView"/>
+/// </summary>
+public static class ItemsViewExtensions
+{
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.EmptyView"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="view"></param>
+	/// <returns>ItemsView with Empty View</returns>
+	public static TItemsView EmptyView<TItemsView>(this TItemsView itemsView, object view) where TItemsView : ItemsView
+	{
+		itemsView.EmptyView = view;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.EmptyViewTemplate"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="view"></param>
+	/// <returns>ItemsView with Empty View Template</returns>
+	public static TItemsView EmptyViewTemplate<TItemsView>(this TItemsView itemsView, DataTemplate view) where TItemsView : ItemsView
+	{
+		itemsView.EmptyViewTemplate = view;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.ItemsSource"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="itemsSource"></param>
+	/// <returns>ItemsView with ItemSource</returns>
+	public static TItemsView ItemsSource<TItemsView>(this TItemsView itemsView, IEnumerable itemsSource) where TItemsView : ItemsView
+	{
+		itemsView.ItemsSource = itemsSource;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.HorizontalScrollBarVisibility"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="visibility"></param>
+	/// <returns>ItemsView with updated Horiztonal Scroll Bar Visibility</returns>
+	public static TItemsView HorizontalScrollBarVisibility<TItemsView>(this TItemsView itemsView, ScrollBarVisibility visibility) where TItemsView : ItemsView
+	{
+		itemsView.HorizontalScrollBarVisibility = visibility;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.VerticalScrollBarVisibility"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="visibility"></param>
+	/// <returns>ItemsView with updated Vertical Scroll Bar Visibility</returns>
+	public static TItemsView VerticalScrollBarVisibility<TItemsView>(this TItemsView itemsView, ScrollBarVisibility visibility) where TItemsView : ItemsView
+	{
+		itemsView.VerticalScrollBarVisibility = visibility;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.VerticalScrollBarVisibility"/> and <see cref="ItemsView.HorizontalScrollBarVisibility"/> properties
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="visibility"></param>
+	/// <returns>ItemsView with updated Horiztonal + Vertical Scroll Bar Visibility</returns>
+	public static TItemsView ScrollBarVisibility<TItemsView>(this TItemsView itemsView, ScrollBarVisibility visibility) where TItemsView : ItemsView
+	{
+		return itemsView.HorizontalScrollBarVisibility(visibility).VerticalScrollBarVisibility(visibility);
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.RemainingItemsThreshold"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="threshold"></param>
+	/// <returns>ItemsView with updated Remaining Items Threshold</returns>
+	public static TItemsView RemainingItemsThreshold<TItemsView>(this TItemsView itemsView, int threshold) where TItemsView : ItemsView
+	{
+		itemsView.RemainingItemsThreshold = threshold;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.RemainingItemsThresholdReachedCommand"/>  ans <see cref="ItemsView.RemainingItemsThresholdReachedCommandParameter"/>properties
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="command"></param>
+	/// <param name="parameter"></param>
+	/// <returns>ItemsView with updated Remaining Items Threshold Reached Command + CommandParameter</returns>
+	public static TItemsView RemainingItemsThresholdReachedCommand<TItemsView>(this TItemsView itemsView, ICommand command, object? parameter) where TItemsView : ItemsView
+	{
+		return itemsView.RemainingItemsThresholdReachedCommand(command).RemainingItemsThresholdReachedCommandParameter(parameter);
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.RemainingItemsThresholdReachedCommand"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="command"></param>
+	/// <returns>ItemsView with updated Remaining Items Threshold Reached Command</returns>
+	public static TItemsView RemainingItemsThresholdReachedCommand<TItemsView>(this TItemsView itemsView, ICommand command) where TItemsView : ItemsView
+	{
+		itemsView.RemainingItemsThresholdReachedCommand = command;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.RemainingItemsThresholdReachedCommandParameter"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="parameter"></param>
+	/// <returns>ItemsView with updated Remaining Items Threshold Reached Command Parameter</returns>
+	public static TItemsView RemainingItemsThresholdReachedCommandParameter<TItemsView>(this TItemsView itemsView, object? parameter) where TItemsView : ItemsView
+	{
+		itemsView.RemainingItemsThresholdReachedCommandParameter = parameter;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.ItemTemplate"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="template"></param>
+	/// <returns>ItemsView with updated Item Template</returns>
+	public static TItemsView ItemTemplate<TItemsView>(this TItemsView itemsView, DataTemplate template) where TItemsView : ItemsView
+	{
+		itemsView.ItemTemplate = template;
+		return itemsView;
+	}
+
+	/// <summary>
+	/// Assigns the <see cref="ItemsView.ItemsUpdatingScrollMode"/> property
+	/// </summary>
+	/// <typeparam name="TItemsView"></typeparam>
+	/// <param name="itemsView"></param>
+	/// <param name="mode"></param>
+	/// <returns>ItemsView with updated ItemsUpdatingScrollMode</returns>
+	public static TItemsView ItemsUpdatingScrollMode<TItemsView>(this TItemsView itemsView, ItemsUpdatingScrollMode mode) where TItemsView : ItemsView
+	{
+		itemsView.ItemsUpdatingScrollMode = mode;
+		return itemsView;
+	}
+}
