78a4320fdc313ccb7041550acd8b5c1768544ca2
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-04-13 13:53:18 -0700

    Re-Add `CommunityToolkit.Maui` to Sample App (#54)
    * Add `CommunityToolkit.Maui` v1.0.0-pre9
    
    * Enable `UseMauiCommunityToolkit()`

--- samples/CommunityToolkit.Maui.Markup.Sample/Pages/SettingsPage.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/Pages/SettingsPage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using CommunityToolkit.Maui.Behaviors;
 using CommunityToolkit.Maui.Markup.Sample.Constants;
 using CommunityToolkit.Maui.Markup.Sample.Pages.Base;
 using CommunityToolkit.Maui.Markup.Sample.Services;
@@ -33,7 +34,6 @@ class SettingsPage : BaseContentPage<SettingsViewModel>
 					.Placeholder($"Provide a value between {SettingsService.MinimumStoriesToFetch} and {SettingsService.MaximumStoriesToFetch}", Colors.Grey)
 					.LayoutFlags(AbsoluteLayoutFlags.XProportional | AbsoluteLayoutFlags.WidthProportional)
 					.LayoutBounds(0.5, 45, 0.8, 40)
-					/* Temporarily remove this until CommunityTookit.Maui v1.0.0-pre9 is released
 					.Behaviors(new NumericValidationBehavior
 					{
 						Flags = ValidationFlags.ValidateOnValueChanged,
@@ -41,7 +41,7 @@ class SettingsPage : BaseContentPage<SettingsViewModel>
 						MaximumValue = SettingsService.MaximumStoriesToFetch,
 						InvalidStyle = new Style<Entry>(Entry.TextColorProperty, Colors.Red),
 						ValidStyle = new Style<Entry>(Entry.TextColorProperty, ColorConstants.PrimaryTextColor),
-					}) */
+					})
 					.Bind(Entry.TextProperty, nameof(SettingsViewModel.NumberOfTopStoriesToFetch))
 					.TextCenter(),
 
