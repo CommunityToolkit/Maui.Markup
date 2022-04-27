2408b2c9bbdca299a6bc73e054c8c84abfc02937
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-04-27 10:23:49 -0700

    Update `csproj`

--- samples/CommunityToolkit.Maui.Markup.Sample/Platforms/Windows/App.xaml.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/Platforms/Windows/App.xaml.cs
@@ -7,7 +7,7 @@ namespace CommunityToolkit.Maui.Markup.Sample.Windows;
 
 public partial class App : MauiWinUIApplication
 {
-	public App() => this.InitializeComponent();
+	public App() => InitializeComponent();
 
 	protected override MauiApp CreateMauiApp() => MauiProgram.Create();
 }
