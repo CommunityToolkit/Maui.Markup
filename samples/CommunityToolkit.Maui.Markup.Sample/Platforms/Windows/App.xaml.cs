c28be7e22f6e2167429594000649c6c9865a8ba3
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-05-03 17:07:41 -0700

    Revert to `CommunityToolkit.Maui.Markup.Sample.Windows`

--- samples/CommunityToolkit.Maui.Markup.Sample/Platforms/Windows/App.xaml.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/Platforms/Windows/App.xaml.cs
@@ -1,9 +1,11 @@
-﻿namespace CommunityToolkit.Maui.Markup.Sample.WinUI;
+﻿using Microsoft.UI.Xaml;
+
+namespace CommunityToolkit.Maui.Markup.Sample.Windows;
 
 public partial class App : MauiWinUIApplication
 {
-	public App() => this.InitializeComponent();
+    public App() => this.InitializeComponent();
 
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
 
