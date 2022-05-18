63ea65e2f730e97e317a84fe66cec508837c13d4
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-05-18 13:35:55 -0700

    Update `GesturesExtensions` (#65)

---
+++ src/CommunityToolkit.Maui.Markup.UnitTests/GesturesExtensionsTests.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
+using CommunityToolkit.Maui.Markup.UnitTests.Base;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+[TestFixture(typeof(Label))] // Derived from View
+class GesturesExtensionsTests<TGestureElement> : BaseMarkupTestFixture where TGestureElement : IGestureRecognizers, new()
+{
+	[Test]
+	public void BindClickGestureDefaults()
+	{
+		var gestureElement = new TGestureElement();
+
+		gestureElement.BindClickGesture(nameof(ViewModel.Command));
+
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<ClickGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		BindingHelpers.AssertBindingExists((ClickGestureRecognizer)gestureElement.GestureRecognizers[0], ClickGestureRecognizer.CommandProperty, nameof(ViewModel.Command));
+	}
+
+	[Test]
+	public void BindClickGesturePositionalParameters()
+	{
+		const int numberOfClicks = 2;
+
+		var gestureElement = new TGestureElement();
+		object commandSource = new ViewModel();
+		object parameterSource = new ViewModel();
+
+		gestureElement.BindClickGesture(nameof(ViewModel.Command), commandSource, nameof(ViewModel.Id), parameterSource, numberOfClicks);
+
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<ClickGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.AreEqual(numberOfClicks, ((ClickGestureRecognizer)gestureElement.GestureRecognizers[0]).NumberOfClicksRequired);
+		BindingHelpers.AssertBindingExists((ClickGestureRecognizer)gestureElement.GestureRecognizers[0], ClickGestureRecognizer.CommandProperty, nameof(ViewModel.Command), source: commandSource);
+		BindingHelpers.AssertBindingExists((ClickGestureRecognizer)gestureElement.GestureRecognizers[0], ClickGestureRecognizer.CommandParameterProperty, nameof(ViewModel.Id), source: parameterSource);
+	}
+
+	[Test]
+	public void BindTapGestureDefaults()
+	{
+		var gestureElement = new TGestureElement();
+
+		gestureElement.BindTapGesture(nameof(ViewModel.Command));
+
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<TapGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		BindingHelpers.AssertBindingExists((TapGestureRecognizer)gestureElement.GestureRecognizers[0], TapGestureRecognizer.CommandProperty, nameof(ViewModel.Command));
+	}
+
+	[Test]
+	public void BindTapGesturePositionalParameters()
+	{
+		const int numberOfTaps = 2;
+
+		var gestureElement = new TGestureElement();
+		object commandSource = new ViewModel();
+		object parameterSource = new ViewModel();
+
+		gestureElement.BindTapGesture(nameof(ViewModel.Command), commandSource, nameof(ViewModel.Id), parameterSource, numberOfTaps);
+
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<TapGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.AreEqual(numberOfTaps, ((TapGestureRecognizer)gestureElement.GestureRecognizers[0]).NumberOfTapsRequired);
+		BindingHelpers.AssertBindingExists((TapGestureRecognizer)gestureElement.GestureRecognizers[0], TapGestureRecognizer.CommandProperty, nameof(ViewModel.Command), source: commandSource);
+		BindingHelpers.AssertBindingExists((TapGestureRecognizer)gestureElement.GestureRecognizers[0], TapGestureRecognizer.CommandParameterProperty, nameof(ViewModel.Id), source: parameterSource);
+	}
+
+	[Test]
+	public void ClickGesture()
+	{
+		const int numberOfClicks = 2;
+		int clicks = 0;
+
+		var gestureElement = new TGestureElement();
+
+		gestureElement.ClickGesture(() => clicks++, numberOfClicks);
+		((ClickGestureRecognizer)gestureElement.GestureRecognizers[0]).SendClicked(null, ButtonsMask.Primary);
+
+		Assert.Greater(clicks, 0);
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<ClickGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.AreEqual(numberOfClicks, ((ClickGestureRecognizer)gestureElement.GestureRecognizers[0]).NumberOfClicksRequired);
+	}
+
+	[Test]
+	public void TapGesture()
+	{
+		const int numberOfTaps = 2;
+		int taps = 0;
+
+		var gestureElement = new TGestureElement();
+
+		gestureElement.TapGesture(() => taps++, numberOfTaps);
+		((TapGestureRecognizer)gestureElement.GestureRecognizers[0]).SendTapped(null);
+
+		Assert.Greater(taps, 0);
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<TapGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.AreEqual(numberOfTaps, ((TapGestureRecognizer)gestureElement.GestureRecognizers[0]).NumberOfTapsRequired);
+	}
+
+	[Test]
+	public void BindSwipeGestureDefaults()
+	{
+		var gestureElement = new TGestureElement();
+
+		gestureElement.BindSwipeGesture(nameof(ViewModel.Command));
+
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<SwipeGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		BindingHelpers.AssertBindingExists((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0], SwipeGestureRecognizer.CommandProperty, nameof(ViewModel.Command));
+	}
+
+	[Test]
+	public void BindSwipeGesturePositionalParameters()
+	{
+		const SwipeDirection direction = SwipeDirection.Up;
+		const uint threshold = 2000;
+
+		var gestureElement = new TGestureElement();
+		object commandSource = new ViewModel();
+		object parameterSource = new ViewModel();
+
+		gestureElement.BindSwipeGesture(nameof(ViewModel.Command), commandSource, nameof(ViewModel.Id), parameterSource, direction, threshold);
+
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<SwipeGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.AreEqual(direction, ((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0]).Direction);
+		Assert.AreEqual(threshold, ((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0]).Threshold);
+		BindingHelpers.AssertBindingExists((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0], SwipeGestureRecognizer.CommandProperty, nameof(ViewModel.Command), source: commandSource);
+		BindingHelpers.AssertBindingExists((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0], SwipeGestureRecognizer.CommandParameterProperty, nameof(ViewModel.Id), source: parameterSource);
+	}
+
+	[Test]
+	public void PanGesture()
+	{
+		const int touchPoints = 2;
+		int panCount = 0;
+
+		var gestureElement = new TGestureElement();
+
+		gestureElement.PanGesture(OnPan, touchPoints);
+		((IPanGestureController)gestureElement.GestureRecognizers[0]).SendPan(null, 1, 2, 1);
+
+		Assert.AreEqual(panCount, 1);
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<PanGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.AreEqual(touchPoints, ((PanGestureRecognizer)gestureElement.GestureRecognizers[0]).TouchPoints);
+
+		void OnPan(object? sender, PanUpdatedEventArgs e) => panCount++;
+	}
+
+	[Test]
+	public void PinchGesture()
+	{
+		int pinchCount = 0;
+
+		var gestureElement = new TGestureElement();
+
+		gestureElement.PinchGesture(OnPinch);
+		((IPinchGestureController)gestureElement.GestureRecognizers[0]).SendPinch(null, 2, new Microsoft.Maui.Graphics.Point());
+
+		Assert.AreEqual(pinchCount, 1);
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<PinchGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+
+		void OnPinch(object? sender, PinchGestureUpdatedEventArgs e) => pinchCount++;
+	}
+
+	[Test]
+	public void SwipeGesture()
+	{
+		const SwipeDirection direction = SwipeDirection.Up;
+		const uint threshold = 2000;
+		int swipeCount = 0;
+
+		var gestureElement = new TGestureElement();
+
+		gestureElement.SwipeGesture(OnSwipe, direction, threshold);
+		((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0]).SendSwiped(null, direction);
+
+		Assert.AreEqual(swipeCount, 1);
+		Assert.AreEqual(1, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<SwipeGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.AreEqual(direction, ((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0]).Direction);
+		Assert.AreEqual(threshold, ((SwipeGestureRecognizer)gestureElement.GestureRecognizers[0]).Threshold);
+
+		void OnSwipe(object? sender, SwipedEventArgs e) => swipeCount++;
+	}
+
+	[Test]
+	public void MultipleGestures()
+	{
+		var gestureElement = new TGestureElement();
+
+		gestureElement.PanGesture();
+		gestureElement.SwipeGesture();
+
+		Assert.AreEqual(2, gestureElement.GestureRecognizers.Count);
+		Assert.IsInstanceOf<PanGestureRecognizer>(gestureElement.GestureRecognizers[0]);
+		Assert.IsInstanceOf<SwipeGestureRecognizer>(gestureElement.GestureRecognizers[1]);
+	}
+
+	[Test]
+	public void SupportDerivedFromLabel() // A View
+	{
+		Assert.IsInstanceOf<DerivedFromLabel>(
+			new DerivedFromLabel()
+			.PanGesture());
+	}
+}
+
+class DerivedFromLabel : Label
+{
+}
+
+
+class DerivedFromGestureRecognizer : GestureRecognizer
+{
+}
+
+class ViewModel
+{
+	public Guid Id { get; set; }
+
+	public ICommand? Command { get; set; }
+}
---
+++ src/CommunityToolkit.Maui.Markup/GesturesExtensions.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Platform;
+
+namespace CommunityToolkit.Maui.Markup;
+
+/// <summary>
+/// Extension Methods for Element Gestures
+/// </summary>
+public static class GesturesExtensions
+{
+	/// <summary>Add a <see cref="ClickGestureRecognizer"/> and bind to its Command and (optionally) CommandParameter properties</summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="commandPath">Path to Command Binding</param>
+	/// <param name="commandSource">Binding source for Command Binding</param>
+	/// <param name="parameterPath">If not specified or null, no binding is created for the CommandParameter property</param>
+	/// <param name="parameterSource">Binding source for Command Binding</param>
+	/// <param name="numberOfClicksRequired">Total number of clicks required to trigger <see cref="ICommand"/></param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement BindClickGesture<TGestureElement>(this TGestureElement gestureElement,
+																		string commandPath,
+																		object? commandSource = null,
+																		string? parameterPath = null,
+																		object? parameterSource = null,
+																		int? numberOfClicksRequired = null) where TGestureElement : IGestureRecognizers
+	{
+		var clickGesture = gestureElement.BindGesture<TGestureElement, ClickGestureRecognizer>(commandPath, commandSource, parameterPath, parameterSource);
+
+		if (numberOfClicksRequired is not null)
+		{
+			clickGesture.NumberOfClicksRequired = numberOfClicksRequired.Value;
+		}
+
+		return gestureElement;
+	}
+
+	/// <summary>Add a <see cref="SwipeGestureRecognizer"/> and bind to its Command and (optionally) CommandParameter properties</summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="commandPath">Path to Command Binding</param>
+	/// <param name="commandSource">Binding source for Command Binding</param>
+	/// <param name="parameterPath">If not specified or null, no binding is created for the CommandParameter property</param>
+	/// <param name="parameterSource">Binding source for Command Binding</param>
+	/// <param name="direction">Swipe gesture direction</param>
+	/// <param name="threshold">Minimum swipe distance that will cause the gesture to be recognized</param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement BindSwipeGesture<TGestureElement>(this TGestureElement gestureElement,
+																		string commandPath,
+																		object? commandSource = null,
+																		string? parameterPath = null,
+																		object? parameterSource = null,
+																		Microsoft.Maui.SwipeDirection? direction = null,
+																		uint? threshold = null) where TGestureElement : IGestureRecognizers
+	{
+		var swipeGesture = gestureElement.BindGesture<TGestureElement, SwipeGestureRecognizer>(commandPath, commandSource, parameterPath, parameterSource);
+
+		if (direction is not null)
+		{
+			swipeGesture.Direction = direction.Value;
+		}
+
+		if (threshold is not null)
+		{
+			swipeGesture.Threshold = threshold.Value;
+		}
+
+		return gestureElement;
+	}
+
+	/// <summary>Adds a <see cref="TapGestureRecognizer"/> and bind its Command and (optionally) CommandParameter properties</summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="commandPath">Path to Command Binding</param>
+	/// <param name="commandSource">Binding source for Command Binding</param>
+	/// <param name="parameterPath">If not specified or null, no binding is created for the CommandParameter property</param>
+	/// <param name="parameterSource">Binding source for Command Binding</param>
+	/// <param name="numberOfTapsRequired">Number of taps required to trigger the <see cref="ICommand"/></param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement BindTapGesture<TGestureElement>(this TGestureElement gestureElement,
+																	string commandPath,
+																	object? commandSource = null,
+																	string? parameterPath = null,
+																	object? parameterSource = null,
+																	int? numberOfTapsRequired = null) where TGestureElement : IGestureRecognizers
+	{
+		var tapGesture = gestureElement.BindGesture<TGestureElement, TapGestureRecognizer>(commandPath, commandSource, parameterPath, parameterSource);
+
+		if (numberOfTapsRequired is not null)
+		{
+			tapGesture.NumberOfTapsRequired = numberOfTapsRequired.Value;
+		}
+
+		return gestureElement;
+	}
+
+	/// <summary>
+	/// Adds a <see cref="ClickGestureRecognizer"/> and sets defines its action when clicked
+	/// </summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="onClicked"><see cref="Action"/>invoked once <paramref name="numberOfClicksRequired"/> threshold is reached</param>
+	/// <param name="numberOfClicksRequired">Total number of clicks required to trigger <paramref name="onClicked"/></param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement ClickGesture<TGestureElement>(this TGestureElement gestureElement,
+																	Action onClicked,
+																	int? numberOfClicksRequired = null) where TGestureElement : IGestureRecognizers
+	{
+		var gestureRecognizer = new ClickGestureRecognizer
+		{
+			Command = new Command(onClicked)
+		};
+
+		if (numberOfClicksRequired is not null)
+		{
+			gestureRecognizer.NumberOfClicksRequired = numberOfClicksRequired.Value;
+		}
+
+		gestureElement.GestureRecognizers.Add(gestureRecognizer);
+
+		return gestureElement;
+	}
+
+	/// <summary>
+	/// Adds a <see cref="PanGestureRecognizer"/>
+	/// </summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="onPanUpdated"><see cref="Action"/> that invokes when the <see cref="PanGestureRecognizer.PanUpdated"/> event is invoked</param>
+	/// <param name="touchPoints">Number of touch points in the gesture</param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement PanGesture<TGestureElement>(this TGestureElement gestureElement,
+																EventHandler<PanUpdatedEventArgs>? onPanUpdated = null,
+																int? touchPoints = null) where TGestureElement : IGestureRecognizers
+	{
+		var gestureRecognizer = new PanGestureRecognizer();
+
+		if (onPanUpdated is not null)
+		{
+			gestureRecognizer.PanUpdated += onPanUpdated;
+		}
+
+		if (touchPoints is not null)
+		{
+			gestureRecognizer.TouchPoints = touchPoints.Value;
+		}
+
+		gestureElement.GestureRecognizers.Add(gestureRecognizer);
+
+		return gestureElement;
+	}
+
+	/// <summary>
+	/// Adds a <see cref="PinchGestureRecognizer"/>
+	/// </summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="onPinchGestureUpdated"><see cref="Action"/> that invokes when the <see cref="PinchGestureRecognizer.PinchUpdated"/> event is invoked</param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement PinchGesture<TGestureElement>(this TGestureElement gestureElement,
+																EventHandler<PinchGestureUpdatedEventArgs>? onPinchGestureUpdated = null) where TGestureElement : IGestureRecognizers
+	{
+		var gestureRecognizer = new PinchGestureRecognizer();
+
+		if (onPinchGestureUpdated is not null)
+		{
+			gestureRecognizer.PinchUpdated += onPinchGestureUpdated;
+		}
+
+		gestureElement.GestureRecognizers.Add(gestureRecognizer);
+
+		return gestureElement;
+	}
+
+	/// <summary>
+	/// Adds a <see cref="SwipeGestureRecognizer"/>
+	/// </summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="onSwiped"><see cref="Action"/> that invokes when the <see cref="SwipeGestureRecognizer.Swiped"/> event is invoked</param>
+	/// <param name="direction">Swipe gesture direction</param>
+	/// <param name="threshold">Minimum swipe distance that will cause the gesture to be recognized</param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement SwipeGesture<TGestureElement>(this TGestureElement gestureElement,
+																EventHandler<SwipedEventArgs>? onSwiped = null,
+																Microsoft.Maui.SwipeDirection? direction = null,
+																uint? threshold = null) where TGestureElement : IGestureRecognizers
+	{
+		var gestureRecognizer = new SwipeGestureRecognizer();
+
+		if (onSwiped is not null)
+		{
+			gestureRecognizer.Swiped += onSwiped;
+		}
+
+		if (direction is not null)
+		{
+			gestureRecognizer.Direction = direction.Value;
+		}
+
+		if (threshold is not null)
+		{
+			gestureRecognizer.Threshold = threshold.Value;
+		}
+
+		gestureElement.GestureRecognizers.Add(gestureRecognizer);
+
+		return gestureElement;
+	}
+
+	/// <summary>
+	/// Adds a <see cref="TapGestureRecognizer"/> and sets defines its action when tapped
+	/// </summary>
+	/// <param name="gestureElement">An <see cref="Element"/> implementing <see cref="IGestureRecognizers"/></param>
+	/// <param name="onTapped"><see cref="Action"/>invoked once <paramref name="numberOfTapsRequired"/> threshold is reached</param>
+	/// <param name="numberOfTapsRequired">Number of taps required to trigger <paramref name="onTapped"/></param>
+	/// <returns><paramref name="gestureElement"/></returns>
+	public static TGestureElement TapGesture<TGestureElement>(this TGestureElement gestureElement,
+																Action onTapped,
+																int? numberOfTapsRequired = null) where TGestureElement : IGestureRecognizers
+	{
+		var gestureRecognizer = new TapGestureRecognizer
+		{
+			Command = new Command(onTapped)
+		};
+
+		if (numberOfTapsRequired is not null)
+		{
+			gestureRecognizer.NumberOfTapsRequired = numberOfTapsRequired.Value;
+		}
+
+		gestureElement.GestureRecognizers.Add(gestureRecognizer);
+
+		return gestureElement;
+	}
+
+	static TGestureRecognizer BindGesture<TGestureElement, TGestureRecognizer>(
+		this TGestureElement gestureElement,
+		string commandPath,
+		object? commandSource = null,
+		string? parameterPath = null,
+		object? parameterSource = null) where TGestureElement : IGestureRecognizers
+										where TGestureRecognizer : BindableObject, IGestureRecognizer, new()
+	{
+		var gestureRecognizer = new TGestureRecognizer().BindCommand(commandPath, commandSource, parameterPath, parameterSource);
+		gestureElement.GestureRecognizers.Add(gestureRecognizer);
+
+		return gestureRecognizer;
+	}
+}
