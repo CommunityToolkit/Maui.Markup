fe779ce714a06f14bde5a8e18eadfed13f1fbeaf
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-08-11 23:21:00 +0100

    Added some sample usage

--- samples/CommunityToolkit.Maui.Markup.Sample/Views/News/StoryDataTemplate.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/Views/News/StoryDataTemplate.cs
@@ -24,12 +24,14 @@ class StoryDataTemplate : DataTemplate
 				.Row(Row.Title)
 				.Font(size: 16).DynamicResource(Label.TextColorProperty, nameof(BaseTheme.PrimaryTextColor))
 				.Top().Padding(10, 0)
-				.Bind(Label.TextProperty, nameof(StoryModel.Title)),
+				.Bind(Label.TextProperty, nameof(StoryModel.Title))
+				.SemanticHint("The title of the news article."),
 
 			new Label().Row(Row.Description)
 				.Font(size: 13).DynamicResource(Label.TextColorProperty, nameof(BaseTheme.SecondaryTextColor))
 				.Paddings(10, 0, 10, 5)
 				.Bind(Label.TextProperty, nameof(StoryModel.Description))
+				.SemanticHint("The description of the news article.")
 		}
 	};
 
