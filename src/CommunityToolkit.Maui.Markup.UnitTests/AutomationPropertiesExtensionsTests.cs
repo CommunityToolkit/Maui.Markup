fd826c4126ba24681ea265ee187b811f96271595
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-09-14 21:26:06 +0100

    Added unit tests

---
+++ src/CommunityToolkit.Maui.Markup.UnitTests/AutomationPropertiesExtensionsTests.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Maui.Markup.UnitTests.Base;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+[TestFixture]
+class AutomationPropertiesExtensionsTests : BaseMarkupTestFixture<Label>
+{
+	[Test]
+	public void AutomationExcludedWithChildrenShouldCorrectlyAssignValue()
+	{
+		const bool isExcluded = true;
+		Bindable.AutomationExcludedWithChildren(isExcluded);
+
+		Assert.That(AutomationProperties.GetExcludedWithChildren(Bindable) == isExcluded);
+	}
+
+	[Test]
+	public void SemanticHintShouldCorrectlyAssignValue()
+	{
+		const bool isInTree = false;
+		Bindable.AutomationIsInAccessibleTree(isInTree);
+
+		Assert.That(AutomationProperties.GetIsInAccessibleTree(Bindable) == isInTree);
+	}
+}
---
+++ src/CommunityToolkit.Maui.Markup.UnitTests/SemanticPropertiesExtensionsTests.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Maui.Markup.UnitTests.Base;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using NUnit.Framework;
+
+namespace CommunityToolkit.Maui.Markup.UnitTests;
+
+[TestFixture]
+class SemanticPropertiesExtensionsTests : BaseMarkupTestFixture<Label>
+{
+	[Test]
+	public void SemanticDescriptionShouldCorrectlyAssignValue()
+	{
+		const string description = "This label does XYZ";
+		Bindable.SemanticDescription(description);
+
+		Assert.That(SemanticProperties.GetDescription(Bindable) == description);
+	}
+
+	[Test]
+	public void SemanticHeadingLevelShouldCorrectlyAssignValue()
+	{
+		const SemanticHeadingLevel headingLevel = SemanticHeadingLevel.Level5;
+		Bindable.SemanticHeadingLevel(headingLevel);
+
+		Assert.That(SemanticProperties.GetHeadingLevel(Bindable) == headingLevel);
+	}
+
+	[Test]
+	public void SemanticHintShouldCorrectlyAssignValue()
+	{
+		const string hint = "This label does XYZ";
+		Bindable.SemanticHint(hint);
+
+		Assert.That(SemanticProperties.GetHint(Bindable) == hint);
+	}
+}
