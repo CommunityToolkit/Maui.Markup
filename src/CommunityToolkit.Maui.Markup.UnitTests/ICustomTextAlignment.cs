8365d92cb3f5dd3b01698ad2e2af556eebfb82df
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-10-28 16:44:30 -0700

    Update ICustomTextAlignment.cs

--- src/CommunityToolkit.Maui.Markup.UnitTests/ICustomTextAlignment.cs
+++ src/CommunityToolkit.Maui.Markup.UnitTests/ICustomTextAlignment.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.Maui;
 
-namespace CommunityToolkit.Maui.UnitTests.Extensions.TextStyle
-{
-	// Ensures custom ITextAlignment interfaces are supported
-	interface ICustomTextAlignment : ITextAlignment
-	{
+namespace CommunityToolkit.Maui.UnitTests.Extensions.TextStyle;
 
-	}
+// Ensures custom ITextAlignment interfaces are supported
+interface ICustomTextAlignment : ITextAlignment
+{
+
 }
