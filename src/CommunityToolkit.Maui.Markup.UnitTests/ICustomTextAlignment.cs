46fd175916521b6f2b0ac2309e19de34f99f22a3
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-10-28 16:50:49 -0700

    Remove Unused Code

--- src/CommunityToolkit.Maui.Markup.UnitTests/ICustomTextAlignment.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Maui;
-
-namespace CommunityToolkit.Maui.UnitTests.Extensions.TextStyle;
-
-// Ensures custom ITextAlignment interfaces are supported
-interface ICustomTextAlignment : ITextAlignment
-{
-
-}
