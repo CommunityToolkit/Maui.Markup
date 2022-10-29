b2a7e887b3f3e022ac1b56dc1f16b546cf510708
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-10-29 19:28:05 +0100

    Improved the XML docs

--- src/CommunityToolkit.Maui.Markup/BindableObjectExtensions.cs
+++ src/CommunityToolkit.Maui.Markup/BindableObjectExtensions.cs
@@ -193,16 +193,16 @@ public static class BindableObjectExtensions
 	}
 
 	/// <summary>
-	/// Set the property value for light and dark theme
+	/// Sets the property values for <paramref name="light"/> and <paramref name="dark"/> themes respectively.
 	/// </summary>
-	/// <typeparam name="TBindable"></typeparam>
-	/// <typeparam name="T"></typeparam>
-	/// <param name="bindable"></param>
-	/// <param name="bindableProperty"></param>
-	/// <param name="light"></param>
-	/// <param name="dark"></param>
-	/// <returns></returns>
-	public static TBindable AppThemeBinding<TBindable, T>(this TBindable bindable, BindableProperty bindableProperty, T light, T dark) where TBindable : BindableObject
+	/// <typeparam name="TBindable">The type of the <paramref name="bindable"/> object.</typeparam>
+	/// <typeparam name="TValue">The value type to be set for <paramref name="light"/> and <paramref name="dark"/> theme.</typeparam>
+	/// <param name="bindable">The bindable object to <c>SetAppTheme</c> on.</param>
+	/// <param name="bindableProperty">The property to apply to the <paramref name="light"/> and <paramref name="dark"/> values to.</param>
+	/// <param name="light">The value to use when the device is configured to use a light theme.</param>
+	/// <param name="dark">The value to use when the device is configured to use a dark theme.</param>
+	/// <returns>The <paramref name="bindable"/> instance to allow for fluently building the user interface.</returns>
+	public static TBindable AppThemeBinding<TBindable, TValue>(this TBindable bindable, BindableProperty bindableProperty, TValue light, TValue dark) where TBindable : BindableObject
 	{
 		bindable.SetAppTheme(bindableProperty, light, dark);
 
@@ -210,14 +210,14 @@ public static class BindableObjectExtensions
 	}
 
 	/// <summary>
-	/// Set the app theme color for light and dark theme
+	/// Set the app theme color for <paramref name="light"/> and <paramref name="dark"/> themes respectively.
 	/// </summary>
-	/// <typeparam name="TBindable"></typeparam>
-	/// <param name="bindable"></param>
-	/// <param name="bindableProperty"></param>
-	/// <param name="light"></param>
-	/// <param name="dark"></param>
-	/// <returns></returns>
+	/// <typeparam name="TBindable">The type of the <paramref name="bindable"/> object.</typeparam>
+	/// <param name="bindable">The bindable object to <c>SetAppThemeColor</c> on.</param>
+	/// <param name="bindableProperty">The property to apply to the <paramref name="light"/> and <paramref name="dark"/> values to.</param>
+	/// <param name="light">The <see cref="Color"/> to use when the device is configured to use a light theme.</param>
+	/// <param name="dark">The <see cref="Color"/> to use when the device is configured to use a dark theme.</param>
+	/// <returns>The <paramref name="bindable"/> instance to allow for fluently building the user interface.</returns>
 	public static TBindable AppThemeColorBinding<TBindable>(this TBindable bindable, BindableProperty bindableProperty, Color light, Color dark) where TBindable : BindableObject
 	{
 		bindable.SetAppThemeColor(bindableProperty, light, dark);
