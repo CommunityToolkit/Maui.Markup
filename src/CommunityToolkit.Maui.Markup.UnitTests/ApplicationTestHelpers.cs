91e3c6fdfd69d1e20bcada32df2baee2ac048cc2
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-10-30 12:44:13 +0000

    XML docs improvement

--- src/CommunityToolkit.Maui.Markup.UnitTests/ApplicationTestHelpers.cs
+++ src/CommunityToolkit.Maui.Markup.UnitTests/ApplicationTestHelpers.cs
@@ -10,7 +10,7 @@ public static class ApplicationTestHelpers
 	/// Performs a test relating to the <see cref="AppTheme"/> of a running application.
 	/// </summary>
 	/// <param name="appTheme">The <see cref="AppTheme"/> value to set the application to.</param>
-	/// <param name="setAppThemeValue">The action to perform in setting the value for the themes. Basically the thing that will be tested.</param>
+	/// <param name="setAppThemeValue">The action to perform in setting the value for the themes. Basically the thing that will be tested. This should return the <see cref="BindableObject"/> that is being tested.</param>
 	/// <param name="assertResult">The action to perform when asserting the result of the <see cref="AppTheme"/> change. Basically the proof of the test.</param>
 	public static void PerformAppThemeBasedTest<TBindable>(
 		AppTheme appTheme,
