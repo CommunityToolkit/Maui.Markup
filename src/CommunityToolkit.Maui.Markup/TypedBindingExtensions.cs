8760bc7b8bd03412d678aa2d47bf048062594b99
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-11-22 13:34:39 -0800

    Update TypedBindingExtensions.cs

--- src/CommunityToolkit.Maui.Markup/TypedBindingExtensions.cs
+++ src/CommunityToolkit.Maui.Markup/TypedBindingExtensions.cs
@@ -7,7 +7,30 @@ namespace CommunityToolkit.Maui.Markup;
 /// </summary>
 public static class TypedBindingExtensions
 {
-	const string bindingContextPath = Binding.SelfPath;
+	/// <summary>Bind to a specified property</summary>
+	public static TBindable Bind<TBindable, TBindingContext, TSource>(
+		this TBindable bindable,
+		BindableProperty targetProperty,
+		Func<TBindingContext, TSource> getter,
+		Action<TBindingContext, TSource>? setter = null,
+		BindingMode? mode = null,
+		string? stringFormat = null,
+		TBindingContext? source = default) where TBindable : BindableObject
+	{
+		bindable.SetBinding(targetProperty, new TypedBinding<TBindingContext, TSource>(result => (getter(result), true), setter, null)
+		{
+			Mode = (setter, mode) switch
+			{
+				(_, not null) => mode.Value, // Always use the provided mode when given
+				(null, null) => BindingMode.OneWay, // When setter is null, binding is read-only; use BindingMode.OneWay to improve performance
+				_ => BindingMode.Default // Default to BindingMode.Default
+			},
+			StringFormat = stringFormat,
+			Source = source,
+		});
+
+		return bindable;
+	}
 
 	/// <summary>Bind to a specified property with inline conversion</summary>
 	public static TBindable Bind<TBindable, TBindingContext, TSource, TDest>(
@@ -42,16 +65,22 @@ public static class TypedBindingExtensions
 		return bindable;
 	}
 
-	/// <summary>Bind to a specified property</summary>
-	public static TBindable Bind<TBindable, TBindingContext, TSource>(
+	/// <summary>Bind to a specified property with inline conversion and conversion parameter</summary>
+	public static TBindable Bind<TBindable, TBindingContext, TSource, TParam, TDest>(
 		this TBindable bindable,
 		BindableProperty targetProperty,
 		Func<TBindingContext, TSource> getter,
 		Action<TBindingContext, TSource>? setter = null,
 		BindingMode? mode = null,
+		Func<TSource?, TParam?, TDest>? convert = null,
+		Func<TDest?, TParam?, TSource>? convertBack = null,
+		TParam? converterParameter = default,
 		string? stringFormat = null,
-		TBindingContext? source = default) where TBindable : BindableObject
+		TBindingContext? source = default,
+		TDest? targetNullValue = default,
+		TDest? fallbackValue = default) where TBindable : BindableObject
 	{
+		var converter = new FuncConverter<TSource, TDest, TParam>(convert, convertBack);
 		bindable.SetBinding(targetProperty, new TypedBinding<TBindingContext, TSource>(result => (getter(result), true), setter, null)
 		{
 			Mode = (setter, mode) switch
@@ -60,11 +89,14 @@ public static class TypedBindingExtensions
 				(null, null) => BindingMode.OneWay, // When setter is null, binding is read-only; use BindingMode.OneWay to improve performance
 				_ => BindingMode.Default // Default to BindingMode.Default
 			},
+			Converter = converter,
+			ConverterParameter = converterParameter,
 			StringFormat = stringFormat,
 			Source = source,
+			TargetNullValue = targetNullValue,
+			FallbackValue = fallbackValue
 		});
 
 		return bindable;
 	}
 }
-
