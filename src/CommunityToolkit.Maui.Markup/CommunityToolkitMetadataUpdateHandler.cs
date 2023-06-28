b91d188371fdae4ddc2476287a0923d33fd99a40
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-06-27 18:44:34 -0700

    Update CommunityToolkitMetadataUpdateHandler.cs

--- src/CommunityToolkit.Maui.Markup/CommunityToolkitMetadataUpdateHandler.cs
+++ src/CommunityToolkit.Maui.Markup/CommunityToolkitMetadataUpdateHandler.cs
@@ -29,5 +29,6 @@ public interface ICommunityToolkitHotReloadHandler
 	/// <summary>
 	/// Executes when C# Hot Reload is invoked
 	/// </summary>
+	/// <param name="types">A <see cref="IReadOnlyList{Type}"/> contianing the types that have been changed in code.</param>
 	void OnHotReload(IReadOnlyList<Type> types);
 }
