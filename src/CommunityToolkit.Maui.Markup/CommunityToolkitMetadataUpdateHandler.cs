5a5f7af100f0e2725bd8de96ac2ffeba4210b0a8
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-06-28 15:42:46 -0700

    Add `ICommunityToolkitHotReloadHandler.cs`

--- src/CommunityToolkit.Maui.Markup/CommunityToolkitMetadataUpdateHandler.cs
+++ src/CommunityToolkit.Maui.Markup/CommunityToolkitMetadataUpdateHandler.cs
@@ -20,15 +20,3 @@ static class CommunityToolkitMetadataUpdateHandler
 	}
 }
 #endif
-
-/// <summary>
-/// Interface for handling C# Hot Reload requests
-/// </summary>
-public interface ICommunityToolkitHotReloadHandler
-{
-	/// <summary>
-	/// Executes when C# Hot Reload is invoked
-	/// </summary>
-	/// <param name="types">A <see cref="IReadOnlyList{Type}"/> contianing the types that have been changed in code.</param>
-	void OnHotReload(IReadOnlyList<Type> types);
-}
