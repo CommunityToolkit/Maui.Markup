f6832572074637c6bc403646100f6fd50caf7c62
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2023-06-27 18:31:39 -0700

    Update HotReloadHandler.cs

--- samples/CommunityToolkit.Maui.Markup.Sample/HotReloadHandler.cs
+++ samples/CommunityToolkit.Maui.Markup.Sample/HotReloadHandler.cs
@@ -1,4 +1,4 @@
-﻿using CommunityToolkit.Maui.Alerts;
+﻿using System.Diagnostics.CodeAnalysis;
 
 namespace CommunityToolkit.Maui.Markup.Sample;
 
@@ -6,6 +6,56 @@ class HotReloadHandler : ICommunityToolkitHotReloadHandler
 {
 	public async void OnHotReload(IReadOnlyList<Type> types)
 	{
-		await Toast.Make("Hot Reload Executed").Show();
+		if (Application.Current?.MainPage is not Page mainPage)
+		{
+			return;
+		}
+
+		foreach (var type in types)
+		{
+			if (type.IsSubclassOf(typeof(Page)))
+			{
+				if (mainPage is AppShell shell)
+				{
+					var visiblePage = shell.CurrentPage;
+
+					await mainPage.Dispatcher.DispatchAsync(async () =>
+					{
+						await Shell.Current.GoToAsync(type.Name, false);
+						Shell.Current.Navigation.RemovePage(visiblePage);
+					});
+
+					break;
+				}
+				else
+				{
+					if (TryGetModalStackPage(out var modalPage))
+					{
+						await mainPage.Dispatcher.DispatchAsync(async () =>
+						{
+							await mainPage.Navigation.PopModalAsync(false);
+							await mainPage.Navigation.PushModalAsync(modalPage, false);
+						});
+					}
+					else
+					{
+						await mainPage.Dispatcher.DispatchAsync(async () =>
+						{
+							await mainPage.Navigation.PopAsync(false);
+							await mainPage.Navigation.PushAsync(modalPage, false);
+						});
+					}
+
+					break;
+				}
+			}
+		}
+	}
+
+
+	static bool TryGetModalStackPage([NotNullWhen(true)] out Page? page)
+	{
+		page = Application.Current?.MainPage?.Navigation.ModalStack.LastOrDefault();
+		return page is not null;
 	}
 }
