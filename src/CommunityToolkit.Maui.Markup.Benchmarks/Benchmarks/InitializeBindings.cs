5d9ab902936b1b51185e57106797d646fe715393
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-06-21 20:49:03 +0200

    Add Performance Benchmarks (#287)
    * Add Benchmarks
    
    * Add `BenchmarkDotNet.Artifacts/` to `.gitignore`
    
    * Add Benchmarks to Build Pipeline
    
    * Update .gitignore
    
    * Only publish artifacts on Windows
    
    * Segregate View-to-ViewModel Bindings from ViewModel-To-View Bindings
    
    * Add missing setters
    
    * Update azure-pipelines.yml
    
    * Update Pipeline Order + Display Name
    
    * Move Benchmarks + Sample App to Separate Job Pipeline
    
    Running these in parallel optimizes our pipeline speeds
    
    * Update azure-pipelines.yml
    
    * Add `CommunityToolkit.Maui.Markup.Benchmarks.csproj`
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update Benchmark CI
    
    * Use `--project`
    
    * Update azure-pipelines.yml
    
    * Publish Benchmark Results to `$(Build.ArtifactStagingDirectory)`
    
    * Disable Code Signing
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml
    
    * Update azure-pipelines.yml

---
+++ src/CommunityToolkit.Maui.Markup.Benchmarks/Benchmarks/InitializeBindings.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CommunityToolkit.Maui.Markup.Benchmarks;
+
+[MemoryDiagnoser]
+public class InitializeBindings : BaseTest
+{
+	readonly Label defaultBindingsLabel = new()
+	{
+		BindingContext = new LabelViewModel()
+	};
+	
+	readonly Label defaultMarkupBindingsLabel = new()
+	{
+		BindingContext = new LabelViewModel()
+	};
+	
+	readonly Label typedMarkupBindingsLabel = new()
+	{
+		BindingContext = new LabelViewModel()
+	};
+
+	[Benchmark(Baseline = true)]
+	public void InitializeDefaultBindings()
+	{
+		defaultBindingsLabel.SetBinding(Label.TextProperty, nameof(LabelViewModel.Text), mode: BindingMode.TwoWay);
+		defaultBindingsLabel.SetBinding(Label.TextColorProperty, nameof(LabelViewModel.TextColor), mode: BindingMode.TwoWay);
+	}
+	
+	[Benchmark]
+	public void InitializeDefaultBindingsMarkup()
+	{
+		defaultMarkupBindingsLabel
+			.Bind(Label.TextProperty, nameof(LabelViewModel.Text), mode: BindingMode.TwoWay)
+			.Bind(Label.TextColorProperty, nameof(LabelViewModel.TextColor), mode: BindingMode.TwoWay);
+	}
+	
+	[Benchmark]
+	public void InitializeTypedBindingsMarkup()
+	{
+		typedMarkupBindingsLabel
+			.Bind(Label.TextProperty,
+				getter: (LabelViewModel vm) => vm.Text,
+				setter: (LabelViewModel vm, string text) => vm.Text = text,
+				mode: BindingMode.TwoWay)
+			.Bind(Label.TextColorProperty,
+				getter: (LabelViewModel vm) => vm.TextColor,
+				setter: (LabelViewModel vm, Color textColor) => vm.TextColor = textColor,
+				mode: BindingMode.TwoWay);
+	}
+}
---
+++ src/CommunityToolkit.Maui.Markup.Benchmarks/LabelViewModel.cs
@@ -0,0 +1,12 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CommunityToolkit.Maui.Markup.Benchmarks;
+
+public partial class LabelViewModel : ObservableObject
+{
+	[ObservableProperty]
+	string text = string.Empty;
+
+	[ObservableProperty]
+	Color textColor = Colors.Black;
+}
