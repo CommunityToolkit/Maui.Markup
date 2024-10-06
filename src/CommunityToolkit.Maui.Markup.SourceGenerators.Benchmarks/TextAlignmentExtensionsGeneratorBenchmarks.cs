696fbf3df15181f0f4e61fe9437bea7c784c7112
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-10-06 15:41:29 -0700

    Add Benchmarks

---
+++ src/CommunityToolkit.Maui.Markup.SourceGenerators.Benchmarks/TextAlignmentExtensionsGeneratorBenchmarks.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+using CommunityToolkit.Maui.Markup.SourceGenerators.UnitTests;
+
+namespace CommunityToolkit.Maui.Markup.SourceGenerators.Benchmarks;
+
+[MemoryDiagnoser]
+public class TextAlignmentExtensionsGeneratorBenchmarks
+{
+	readonly TextAlignmentExtensionsGeneratorTests textAlignmentExtensionsGeneratorTests = new();
+
+	[Benchmark]
+	public Task VerifyGeneratedSource_WhenClassIsGeneric()
+	{
+		return textAlignmentExtensionsGeneratorTests.VerifyGeneratedSource_WhenClassIsGeneric();
+	}
+
+	[Benchmark]
+	public Task VerifyNoErrorsWhenUseMauiCommunityToolkitHasAdditonalWhitespace()
+	{
+		return textAlignmentExtensionsGeneratorTests.VerifyGeneratedSource_WhenClassImplementsITextAlignmentInterface();
+	}
+}
